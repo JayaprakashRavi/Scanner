bb041c454e35788d5669ee55c174a69bf92ba564
JayaprkashRavi <rjayaprakashmech1997@gmail.com>
2022-11-20 20:01:05 +0530

    msg

---
+++ src/org/Test/Sample.java
@@ -0,0 +1,27 @@
+package org.Test;
+
+import java.util.Scanner;
+
+public class Sample {
+public static void main(String[] args) {
+	 Scanner sc = new Scanner (System.in);
+	System.out.println("enter the value");
+	byte age = sc.nextByte();
+	System.out.println("enter the age"+age );
+	System.out.println("enter the year");
+	
+	short year = sc.nextShort();
+	System.out.println ("enter the year "+year);
+	System.out.println("enter the pincode");
+	int pincode = sc.nextInt();
+	System.out.println("enter the pincode"+pincode);
+	System.out.println("mobile ");
+	long mobile = sc.nextLong();
+	System.out.println("enter the mobile no");
+	System.out.println("salary is ");
+	double salary = sc.nextDouble();
+	System.out.println();
+	
+	
+}
+}
